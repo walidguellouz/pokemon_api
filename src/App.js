74c76bfd3d9dcb417b555f7023ca100a69f8c7aa
M Oualid GUELLOUZ <oualid.guellouz@gmail.com>
2023-03-13 15:03:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fetchPokemonArray, setFetchPokemonArray] = useState([])
+
+
+
+  // - fetch Vanilla JS
+  const fetchPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+    .then(response => response.json())
+    .then(response => {
+        setFetchPokemonArray(response.results)
+        console.log(response.results[0].name)
+      }).
+      catch(error => console.log(error))
+  }
+
+  
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon API</h1>
+      <button onClick={fetchPokemon}>fetch pokemon</button>
+      
+      <p>{JSON.stringify(fetchPokemon)}</p>
+      <h4>Fetch pokemon</h4>
+      <table>
+        <thead>
+          <tr>
+            
+            <th>Pokemon Name</th>
+            <th>Full Name</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {
+            fetchPokemonArray.map((pokemon, index) => {
+              return (
+                <tr key={index}>
+                  <td>{pokemon.name}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      <hr />
+      
     </div>
   );
 }
